f9e7120d97ce6d8b1b405e5379cf2bf820686d30
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-12 15:15:25 +0100

    Set up gson matching and test response

--- app/src/main/java/com/oluwatimilehin/cryptoconverter/MainActivity.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.oluwatimilehin.cryptoconverter
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/test/java/com/oluwatimilehin/cryptoconverter/GSONMappingTests.kt
@@ -0,0 +1,31 @@
+package com.oluwatimilehin.cryptoconverter
+
+import com.google.gson.Gson
+import com.oluwatimilehin.cryptoconverter.data.ETHExchangeRate
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Created by Oluwatimilehin on 12/10/2017.
+ * oluwatimilehinadeniran@gmail.com.
+ */
+class GSONMappingTests {
+    @Test
+    fun gsonMapping(){
+        val jsonString : String = "{\n" +
+                "\"ETH\": {\n" +
+                "\"USD\": 1.13,\n" +
+                "\"EUR\": 1.04,\n" +
+                "\"NGN\": 290.85,\n" +
+                "\"CAD\": 1.63,\n" +
+                "\"AUD\": 1.6,\n" +
+                "\"JPY\": 133.43\n" +
+                "}\n" +
+                "}";
+
+        val gson: Gson = Gson()
+        val eth: ETHExchangeRate? = gson.fromJson(jsonString, ETHExchangeRate::class.java)
+
+        assertTrue(eth?.exchangeRates?.size == 6);
+    }
+}
