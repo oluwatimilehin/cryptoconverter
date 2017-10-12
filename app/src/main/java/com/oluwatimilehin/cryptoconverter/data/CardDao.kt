ef1a3792403eceb31426c1c81357752b0c3e7c4a
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-12 19:53:29 +0100

    Define the DAOs for both tables

--- app/src/main/java/com/oluwatimilehin/cryptoconverter/data/CardDao.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/data/CardDao.kt
@@ -1,6 +1,26 @@
 package com.oluwatimilehin.cryptoconverter.data
 
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import io.reactivex.Flowable
+
 /**
  * Created by Oluwatimilehin on 12/10/2017.
  * oluwatimilehinadeniran@gmail.com.
  */
+@Dao
+interface CardDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(card: Card)
+
+    @Query("SELECT * from cards")
+    fun getAllCards(): Flowable<List<Card>>
+
+    @Query("UPDATE cards SET amount = :newAmount WHERE from = :from AND to = :to")
+    fun updateAmount(newAmount: Double, from: String, to: String);
+
+    @Query("DELETE from cards")
+    fun deleteAllUsers();
+}
--- app/src/main/java/com/oluwatimilehin/cryptoconverter/data/CurrencyDao.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/data/CurrencyDao.kt
@@ -1,6 +1,23 @@
 package com.oluwatimilehin.cryptoconverter.data
 
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import android.arch.persistence.room.Update
+import io.reactivex.Flowable
+
 /**
  * Created by Oluwatimilehin on 12/10/2017.
  * oluwatimilehinadeniran@gmail.com.
  */
+interface CurrencyDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllCurrencies(currencies: List<Currency>)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun updateAmounts(currencies: List<Currency>)
+
+    @Query("SELECT amount from currencies WHERE from= :from AND to= :to")
+    fun getConversionRate(from: String, to: String): Flowable<Double>;
+
+}
