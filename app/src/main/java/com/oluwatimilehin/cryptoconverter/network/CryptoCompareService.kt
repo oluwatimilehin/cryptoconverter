39461b10a93db136c35dcd971355533e774c1a27
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-12 19:08:32 +0100

    Define factory class for creating service

--- app/src/main/java/com/oluwatimilehin/cryptoconverter/network/CryptoCompareService.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/network/CryptoCompareService.kt
@@ -1,6 +1,9 @@
 package com.oluwatimilehin.cryptoconverter.network
 
 import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -15,4 +18,18 @@ interface CryptoCompareService {
     @GET("data/price?fsym=BTC")
     fun getBTCRates(@Query("tsyms") tsyms: String) : Single<HashMap<String, Double>>
 
+    /**
+     * Companion object to create the CryptoCompareService
+     */
+    companion object Factory {
+        fun create() : CryptoCompareService{
+            val retrofit = Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .baseUrl("https://min-api.cryptocompare.com/")
+                    .build()
+
+            return  retrofit.create(CryptoCompareService::class.java)
+        }
+    }
 }
