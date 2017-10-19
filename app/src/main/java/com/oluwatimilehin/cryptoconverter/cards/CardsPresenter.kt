b8ea657d8f407652da6a48466fecac5df518ccd6
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-19 20:16:56 +0100

    Clear presenter

--- app/src/main/java/com/oluwatimilehin/cryptoconverter/cards/CardsPresenter.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/cards/CardsPresenter.kt
@@ -1,16 +1,10 @@
 package com.oluwatimilehin.cryptoconverter.cards
 
-import android.text.TextUtils
-import com.mynameismidori.currencypicker.ExtendedCurrency
 import com.oluwatimilehin.cryptoconverter.App
 import com.oluwatimilehin.cryptoconverter.data.Currency
 import com.oluwatimilehin.cryptoconverter.network.CryptoCompareService
 import io.reactivex.Scheduler
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
 
 
@@ -35,52 +29,7 @@ class CardsPresenter : CardsContract.Presenter {
     }
 
     override fun loadDataFromApi() {
-        val currencies = ExtendedCurrency.CURRENCIES
-
-        val list: ArrayList<String> = ArrayList();
-
-        for(item: ExtendedCurrency in currencies){
-            list.add(item.code)
-        }
-
         val cryptoApi: CryptoCompareService = CryptoCompareService.create()
-        val countries: String = TextUtils.join(",", list)
-
-        val btcRates: Single<List<Currency>> = cryptoApi.getBTCRates(countries)
-                .subscribeOn(scheduler)
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnError { e -> e.printStackTrace() }
-                .flatMap { result: HashMap<String, Double> ->
-                    return@flatMap Single.just(createCurrencyObjects
-                    ("BTC",
-                            result))
-                }
-
-
-        val ethRates: Single<List<Currency>> = cryptoApi.getETHRates(countries)
-                .subscribeOn(scheduler)
-                .observeOn(AndroidSchedulers.mainThread())
-                .flatMap { result: HashMap<String, Double> ->
-                    return@flatMap Single.just(createCurrencyObjects("ETH", result))
-                }
-                .doOnError { e -> e.printStackTrace() }
-
-
-        val apiCall: Disposable = Single.zip(btcRates, ethRates, BiFunction { ethlist:
-                                                                              List<Currency>,
-                                                                              btcList:
-                                                                              List<Currency> ->
-            val list: ArrayList<Currency> = ArrayList();
-            list.addAll(ethlist)
-            list.addAll(btcList)
-            return@BiFunction list
-        })
-                .doOnSuccess{list ->
-                    saveDataInDb(list)}
-                .doOnError{e -> e.printStackTrace()}
-                .subscribe()
-
-        disposables.add(apiCall)
     }
 
     fun createCurrencyObjects(from: String, map: HashMap<String, Double>): List<Currency> {
