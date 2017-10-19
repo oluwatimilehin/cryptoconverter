1c28f7bd12868bf2b88a75e99cd298bbe8567a94
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-19 20:42:02 +0100

    Add currency constants

---
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/data/Constants.kt
@@ -0,0 +1,30 @@
+package com.oluwatimilehin.cryptoconverter.data
+
+/**
+ * Created by Oluwatimilehin on 19/10/2017.
+ * oluwatimilehinadeniran@gmail.com.
+ */
+class Constants{
+    val listOfCurrencies: List<String> = listOf("EUR",
+            "USD",
+            "GBP",
+            "CZK",
+            "TRY",
+            "JPY",
+            "NGN",
+            "AED",
+            "AFN",
+            "ARS",
+            "AUD",
+            "BDT",
+            "BGN",
+            "BHD",
+            "BND",
+            "BOB",
+            "BRL",
+            "BTN",
+            "CAD",
+            "CHF",
+            "CLP",
+            "CNY")
+}
