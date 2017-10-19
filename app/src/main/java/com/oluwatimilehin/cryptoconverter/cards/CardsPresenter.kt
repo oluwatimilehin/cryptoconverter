0b145477cb014e0ccf2dff22c99152eca52d4326
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-20 00:05:37 +0100

    Set up disposable for api call

--- app/src/main/java/com/oluwatimilehin/cryptoconverter/cards/CardsPresenter.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/cards/CardsPresenter.kt
@@ -1,10 +1,17 @@
 package com.oluwatimilehin.cryptoconverter.cards
 
+import android.content.Context
+import android.widget.Toast
 import com.oluwatimilehin.cryptoconverter.App
+import com.oluwatimilehin.cryptoconverter.data.Constants
 import com.oluwatimilehin.cryptoconverter.data.Currency
+import com.oluwatimilehin.cryptoconverter.data.ExchangeRate
 import com.oluwatimilehin.cryptoconverter.network.CryptoCompareService
 import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 
@@ -12,11 +19,12 @@ import io.reactivex.schedulers.Schedulers
  * Created by Oluwatimilehin on 13/10/2017.
  * oluwatimilehinadeniran@gmail.com.
  */
-class CardsPresenter : CardsContract.Presenter {
+class CardsPresenter(paramContext: Context) : CardsContract.Presenter {
 
     lateinit var view: CardsContract.View;
     val disposables: CompositeDisposable = CompositeDisposable();
     val scheduler: Scheduler = Schedulers.io()
+    val context = paramContext
 
     override fun attachView(view: CardsContract.View) {
         this.view = view
@@ -30,6 +38,28 @@ class CardsPresenter : CardsContract.Presenter {
 
     override fun loadDataFromApi() {
         val cryptoApi: CryptoCompareService = CryptoCompareService.create()
+
+
+        val apiCallDisposable: Disposable = cryptoApi.getRates(Constants.currenciesString)
+                .subscribeOn(Schedulers.io())
+                .doOnError{ e -> e.printStackTrace()}
+                .flatMap { rates: ExchangeRate ->
+                    val combinedList: MutableList<Currency> = ArrayList()
+
+                    combinedList.addAll(createCurrencyObjects("BTC", rates.btcRates ))
+                    combinedList.addAll(createCurrencyObjects("ETH", rates.ethRates))
+
+                    return@flatMap  Single.just(combinedList)
+                }
+                .map { result -> saveDataInDb(result)}
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    Toast.makeText(context, "Saved to database", Toast
+                        .LENGTH_SHORT).show()}, { e -> e.printStackTrace()} )
+
+
+        disposables.add(apiCallDisposable)
+
     }
 
     fun createCurrencyObjects(from: String, map: HashMap<String, Double>): List<Currency> {
