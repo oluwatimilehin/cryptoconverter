00537269acdd5ffaad0975001a32ba143d41ed4f
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-24 15:57:31 +0100

    Set up migration with new columns

--- app/src/main/java/com/oluwatimilehin/cryptoconverter/data/AppDatabase.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/data/AppDatabase.kt
@@ -1,14 +1,25 @@
 package com.oluwatimilehin.cryptoconverter.data
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.migration.Migration
 
 /**
  * Created by Oluwatimilehin on 12/10/2017.
  * oluwatimilehinadeniran@gmail.com.
  */
-@Database(entities = arrayOf(Card::class, Currency::class), version = 2)
+@Database(entities = arrayOf(Card::class, Currency::class), version = 3)
 abstract class AppDatabase : RoomDatabase(){
-    abstract fun cardDao(): CardDao;
-    abstract fun currencyDao(): CurrencyDao
+    abstract fun currencyDao(): CurrencyDao;
+
+    companion object {
+        var MIGRATION_2_3: Migration = object: Migration(2, 3) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE currencies ADD COLUMN drawable INTEGER")
+                database.execSQL("ALTER TABLE currencies ADD COLUMN selected INTEGER")
+                database.execSQL("ALTER TABLE currencies ADD COLUMN full_name STRING")
+            }
+        }
+    }
 }
