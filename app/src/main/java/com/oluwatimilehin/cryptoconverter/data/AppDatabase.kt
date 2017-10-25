a0fbdbddc4de7bbcf8dcda030677b62e6ce7b290
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-25 21:51:43 +0100

    Increase db  version

--- app/src/main/java/com/oluwatimilehin/cryptoconverter/data/AppDatabase.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/data/AppDatabase.kt
@@ -7,7 +7,7 @@ import android.arch.persistence.room.RoomDatabase
  * Created by Oluwatimilehin on 12/10/2017.
  * oluwatimilehinadeniran@gmail.com.
  */
-@Database(entities = arrayOf(Card::class, Currency::class), version = 2)
+@Database(entities = arrayOf(Card::class, Currency::class), version = 3)
 abstract class AppDatabase : RoomDatabase(){
     abstract fun cardDao(): CardDao;
     abstract fun currencyDao(): CurrencyDao
