0d851d92317e17b9164884fae62c30c8d98a422d
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-26 12:19:39 +0100

    Create base test class and refactor code

--- app/src/androidTest/java/com/oluwatimilehin/cryptoconverter/CardDaoTest.kt
+++ app/src/androidTest/java/com/oluwatimilehin/cryptoconverter/CardDaoTest.kt
@@ -1,15 +1,11 @@
 package com.oluwatimilehin.cryptoconverter
 
-import com.oluwatimilehin.cryptoconverter.data.AppDatabase
 import com.oluwatimilehin.cryptoconverter.data.CardDao
-import com.oluwatimilehin.cryptoconverter.data.CurrencyDao
 
 /**
  * Created by Oluwatimilehin on 26/10/2017.
  * oluwatimilehinadeniran@gmail.com.
  */
-class CardDaoTest{
-    lateinit var appDb: AppDatabase
-    lateinit var currencyDao: CurrencyDao
+class CardDaoTest : BaseDaoTest(){
     lateinit var cardDao: CardDao
 }
