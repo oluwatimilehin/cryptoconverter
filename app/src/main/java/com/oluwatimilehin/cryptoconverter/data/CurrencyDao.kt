a9399c89ea65002a763939bfeda0b13fe91d742a
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-26 22:28:10 +0100

    Return single from query

--- app/src/main/java/com/oluwatimilehin/cryptoconverter/data/CurrencyDao.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/data/CurrencyDao.kt
@@ -5,6 +5,7 @@ import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import io.reactivex.Flowable
+import io.reactivex.Single
 
 /**
  * Created by Oluwatimilehin on 12/10/2017.
@@ -17,9 +18,12 @@ interface CurrencyDao {
     fun insertAllCurrencies(currencies: List<Currency>)
 
     @Query("SELECT amount from currencies WHERE \"from\" = :from AND \"to\" = :to")
-    fun getConversionRate(from: String, to: String): Flowable<Double>
+    fun getConversionRate(from: String, to: String): Single<Double>
 
     @Query("SELECT * from currencies")
     fun getAllCurrencies(): Flowable<List<Currency>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertCurrency(currency: Currency)
 
 }
