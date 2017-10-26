2b81802efa65c341bc8bbf02101aa0505cb4e9a6
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-26 12:10:04 +0100

    Create CardDaoTest class

---
+++ app/src/androidTest/java/com/oluwatimilehin/cryptoconverter/CardDaoTest.kt
@@ -0,0 +1,15 @@
+package com.oluwatimilehin.cryptoconverter
+
+import com.oluwatimilehin.cryptoconverter.data.AppDatabase
+import com.oluwatimilehin.cryptoconverter.data.CardDao
+import com.oluwatimilehin.cryptoconverter.data.CurrencyDao
+
+/**
+ * Created by Oluwatimilehin on 26/10/2017.
+ * oluwatimilehinadeniran@gmail.com.
+ */
+class CardDaoTest{
+    lateinit var appDb: AppDatabase
+    lateinit var currencyDao: CurrencyDao
+    lateinit var cardDao: CardDao
+}
