cb26248ace44cc7602f14bb0bc779429e912e50c
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-30 23:14:08 +0100

    Add base presenter parent class

---
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/BasePresenter.kt
@@ -0,0 +1,20 @@
+package com.oluwatimilehin.cryptoconverter
+
+import com.oluwatimilehin.cryptoconverter.cards.CardsContract
+import com.oluwatimilehin.cryptoconverter.data.CardDao
+import com.oluwatimilehin.cryptoconverter.data.CurrencyDao
+import io.reactivex.Scheduler
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Created by Oluwatimilehin on 30/10/2017.
+ * oluwatimilehinadeniran@gmail.com.
+ */
+abstract class BasePresenter{
+    lateinit var view: CardsContract.View;
+    val disposables: CompositeDisposable = CompositeDisposable();
+    val scheduler: Scheduler = Schedulers.single()
+    lateinit var cardDao: CardDao
+    lateinit var currencyDao: CurrencyDao
+}
