999efe482e052e72d99543c9c06fb31fe699467b
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-30 22:59:28 +0100

    Define contract and presenter for adding card

---
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/addcard/AddCardContract.kt
@@ -0,0 +1,16 @@
+package com.oluwatimilehin.cryptoconverter.addcard
+
+/**
+ * Created by Oluwatimilehin on 30/10/2017.
+ * oluwatimilehinadeniran@gmail.com.
+ */
+interface AddCardContract{
+    interface View{
+        fun cardExistsError()
+        fun saveCardSuccess()
+    }
+
+    interface Presenter{
+        fun saveCard(from: String, to: String)
+    }
+}
---
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/addcard/AddCardPresenter.kt
@@ -0,0 +1,12 @@
+package com.oluwatimilehin.cryptoconverter.addcard
+
+/**
+ * Created by Oluwatimilehin on 30/10/2017.
+ * oluwatimilehinadeniran@gmail.com.
+ */
+class AddCardPresenter : AddCardContract.Presenter{
+    override fun saveCard(from: String, to: String) {
+
+    }
+
+}
