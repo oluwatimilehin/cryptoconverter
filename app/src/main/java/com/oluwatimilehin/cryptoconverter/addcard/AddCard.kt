163c40155936c73297227f2c0f0b4e4146a99b15
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-30 15:10:01 +0100

    Set up 'AddCards' activity

---
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/addcard/AddCard.kt
@@ -0,0 +1,13 @@
+package com.oluwatimilehin.cryptoconverter.addcard
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.oluwatimilehin.cryptoconverter.R
+
+class AddCard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_card)
+    }
+}
