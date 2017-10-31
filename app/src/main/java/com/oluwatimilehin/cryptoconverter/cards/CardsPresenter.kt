0e4bf15ab0985796876f8a6d15414669145e4d8a
Timi <oluwatimilehinadeniran@gmail.com>
2017-10-31 22:59:56 +0100

    Refactoring and documentation

--- app/src/main/java/com/oluwatimilehin/cryptoconverter/cards/CardsPresenter.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/cards/CardsPresenter.kt
@@ -8,6 +8,7 @@ import com.oluwatimilehin.cryptoconverter.data.Currency
 import com.oluwatimilehin.cryptoconverter.data.ExchangeRate
 import com.oluwatimilehin.cryptoconverter.network.CryptoCompareService
 import io.reactivex.Completable
+import io.reactivex.Flowable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 
@@ -16,15 +17,16 @@ import io.reactivex.android.schedulers.AndroidSchedulers
  * Created by Oluwatimilehin on 13/10/2017.
  * oluwatimilehinadeniran@gmail.com.
  */
-class CardsPresenter : CardsContract.Presenter, BasePresenter(){
+class CardsPresenter : CardsContract.Presenter, BasePresenter() {
+
     override fun deleteCard(card: Card) {
-        Completable.fromAction {cardDao.deleteCard(card.from, card.to)}
+        disposables.add(Completable.fromAction { cardDao.deleteCard(card.from, card.to) }
                 .subscribeOn(scheduler)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
                     cardsView.showCardDeleted()
                     loadCards()
-                })
+                }))
     }
 
     override fun addNewCard() {
@@ -32,12 +34,12 @@ class CardsPresenter : CardsContract.Presenter, BasePresenter(){
     }
 
     override fun loadCurrencies() {
-        val cryptoApi: CryptoCompareService = CryptoCompareService.create()
-
         disposables.add(currencyDao.checkIfCurrenciesExist()
                 .subscribeOn(scheduler)
                 .observeOn(AndroidSchedulers.mainThread())
                 .map { currencies ->
+                    //First check if the database is populated with currencies
+                    // values
                     if (currencies.isEmpty()) {
                         cardsView.showEmptyCurrenciesError()
                     } else {
@@ -46,30 +48,12 @@ class CardsPresenter : CardsContract.Presenter, BasePresenter(){
 
                 }
                 .map {
-                    currencyDao.getAllCurrencies()
-                            .subscribeOn(scheduler)
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .map { currencies ->
-                                if (!currencies.isEmpty()) {
-                                    cardsView.currenciesExist()
-                                    updateCards()
-                                            .subscribe()
-                                }
-                            }
+                    //Each time the currency table is updated, this flowable gets notified.
+                    getCurrenciesFlowable()
                             .subscribe()
                 }
                 .map {
-                    cryptoApi.getRates(Constants.currenciesString)
-                            .subscribeOn(scheduler)
-                            .doOnError { cardsView.showApiCallError() }
-                            .flatMap { rates: ExchangeRate ->
-                                val combinedList: MutableList<Currency> = ArrayList()
-
-                                combinedList.addAll(createCurrencyObjects("BTC", rates.btcRates))
-                                combinedList.addAll(createCurrencyObjects("ETH", rates.ethRates))
-
-                                return@flatMap Single.just(combinedList)
-                            }
+                    loadData() //Then get the data from the API
                             .map { result -> saveDataInDb(result) }
                             .observeOn(AndroidSchedulers.mainThread())
                             .subscribe({
@@ -80,6 +64,9 @@ class CardsPresenter : CardsContract.Presenter, BasePresenter(){
 
     }
 
+    /**
+     * Loads all the users cards and populates the recyclerview adapter
+     */
     override fun loadCards() {
         disposables.add(cardDao.getAllCards()
                 .subscribeOn(scheduler)
@@ -92,7 +79,46 @@ class CardsPresenter : CardsContract.Presenter, BasePresenter(){
 
     }
 
-    fun updateCards(): Single<List<Card>> {
+    /**
+     * Helper method that makes the API call and returns a list of all the exchange rates
+     */
+    private fun loadData(): Single<MutableList<Currency>> {
+        val cryptoApi: CryptoCompareService = CryptoCompareService.create()
+
+        return cryptoApi.getRates(Constants.currenciesString)
+                .subscribeOn(scheduler)
+                .doOnError { cardsView.showApiCallError() }
+                .flatMap { rates: ExchangeRate ->
+                    val combinedList: MutableList<Currency> = ArrayList()
+
+                    combinedList.addAll(createCurrencyObjects("BTC", rates.btcRates))
+                    combinedList.addAll(createCurrencyObjects("ETH", rates.ethRates))
+
+                    return@flatMap Single.just(combinedList)
+                }
+    }
+
+    /**
+     * Helper method. Each time the currency table is updated, this observable gets notified with
+     * the new currencies and each card a user has saved gets updated with the new values.
+     */
+    private fun getCurrenciesFlowable(): Flowable<Unit> {
+        return currencyDao.getAllCurrencies()
+                .subscribeOn(scheduler)
+                .observeOn(AndroidSchedulers.mainThread())
+                .map { currencies ->
+                    if (!currencies.isEmpty()) {
+                        cardsView.currenciesExist()
+                        updateCards()
+                                .subscribe()
+                    }
+                }
+    }
+
+    /**
+     * Gets all the cards and updates them with the new currencies
+     */
+    private fun updateCards(): Single<List<Card>> {
         return cardDao.getAllCards()
                 .subscribeOn(scheduler)
                 .observeOn(AndroidSchedulers.mainThread())
@@ -129,14 +155,14 @@ class CardsPresenter : CardsContract.Presenter, BasePresenter(){
         val list = ArrayList<Currency>(0)
 
         for (key in map.keys) {
-            val amount: Double? = map.get(key)
+            val amount: Double? = map[key]
             list.add(Currency(0, from, key, amount!!))
         }
 
         return list
     }
 
-    fun saveDataInDb(list: List<Currency>) {
+    private fun saveDataInDb(list: List<Currency>) {
         currencyDao.insertAllCurrencies(list)
     }
 
