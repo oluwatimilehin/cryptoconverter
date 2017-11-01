f399ab16ce6c3b6c4f9de8bc3966e09d080c9a55
Timi <oluwatimilehinadeniran@gmail.com>
2017-11-01 18:21:50 +0100

    Add activity for currency conversion

---
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/currencyconversion/ConvertAmountActivity.kt
@@ -0,0 +1,13 @@
+package com.oluwatimilehin.cryptoconverter.currencyconversion
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.oluwatimilehin.cryptoconverter.R
+
+class ConvertAmountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_convert_amount)
+    }
+}
