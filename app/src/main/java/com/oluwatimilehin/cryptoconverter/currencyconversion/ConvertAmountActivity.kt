ccdfbd6e360106805c94e0856e7560f1e5ee91ab
Timi <oluwatimilehinadeniran@gmail.com>
2017-11-02 12:46:34 +0100

    Set toolbar text

--- app/src/main/java/com/oluwatimilehin/cryptoconverter/currencyconversion/ConvertAmountActivity.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/currencyconversion/ConvertAmountActivity.kt
@@ -3,11 +3,16 @@ package com.oluwatimilehin.cryptoconverter.currencyconversion
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.oluwatimilehin.cryptoconverter.R
+import kotlinx.android.synthetic.main.toolbar.*
 
 class ConvertAmountActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_convert_amount)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(true)
+        supportActionBar?.title = "Convert Amount"
     }
 }
