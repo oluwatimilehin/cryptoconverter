328806708518131f2b036a338b7ffd69d6d8bbdc
Timi <oluwatimilehinadeniran@gmail.com>
2017-11-02 18:05:39 +0100

    Perform calculation as user enters text

--- app/src/main/java/com/oluwatimilehin/cryptoconverter/currencyconversion/ConversionActivity.kt
+++ app/src/main/java/com/oluwatimilehin/cryptoconverter/currencyconversion/ConversionActivity.kt
@@ -2,12 +2,13 @@ package com.oluwatimilehin.cryptoconverter.currencyconversion
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.jakewharton.rxbinding2.widget.RxTextView
 import com.oluwatimilehin.cryptoconverter.R
 import com.oluwatimilehin.cryptoconverter.data.Constants
 import kotlinx.android.synthetic.main.activity_convert_amount.*
 import kotlinx.android.synthetic.main.toolbar.*
 
-class ConvertAmountActivity : AppCompatActivity() {
+class ConversionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +23,15 @@ class ConvertAmountActivity : AppCompatActivity() {
         fromLabel.text = bundle.getString(Constants.KEY_FROM)
         toLabel.text = bundle.getString(Constants.KEY_TO)
         toField.hint = bundle.getDouble(Constants.KEY_AMOUNT).toString()
+
+        RxTextView.textChanges(fromField)
+                .filter{s -> s.isNotEmpty() }
+                .subscribe({
+                    s -> run{
+                    var amount: Double = s.toString().toDouble()
+                    amount *= bundle.getDouble(Constants.KEY_AMOUNT)
+                    toField.setText(amount.toString())
+                }
+                })
     }
 }
