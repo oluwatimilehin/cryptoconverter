e46e23c991ecff41e29366a455e3a5b768787385
Timi <oluwatimilehinadeniran@gmail.com>
2017-12-06 23:50:11 +0100

    Conversion presenter tests passed

--- app/src/test/java/com/oluwatimilehin/cryptoconverter/presentation/ConversionPresenterTest.kt
+++ app/src/test/java/com/oluwatimilehin/cryptoconverter/presentation/ConversionPresenterTest.kt
@@ -1,16 +1,22 @@
 package com.oluwatimilehin.cryptoconverter.presentation
 
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
 import com.oluwatimilehin.cryptoconverter.conversion.ConversionContract
 import com.oluwatimilehin.cryptoconverter.conversion.ConversionPresenter
 import com.oluwatimilehin.cryptoconverter.utils.ConversionCalculator
 import org.junit.Before
+import org.junit.Test
 import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
 /**
  * Created by Oluwatimilehin on 04/12/2017.
  * oluwatimilehinadeniran@gmail.com.
  */
 class ConversionPresenterTest{
+
     @Mock
     lateinit var view: ConversionContract.View
 
@@ -21,6 +27,28 @@ class ConversionPresenterTest{
 
     @Before
     fun setUp(){
+
+        MockitoAnnotations.initMocks(this)
         presenter = ConversionPresenter(view, calculator)
+    }
+
+    @Test
+    fun typingAnInput_shouldUpdateTheRightField(){
+        //Given
+        whenever(calculator.calculateAmount(any(), any(), any())).thenReturn("11112223")
+
+        //When conversion mode is "from"
+        presenter.setAmount(2333.8)
+        presenter.convertAmount("64","from")
+
+        //Then the to field should be updated
+        verify(view).updateToField(any())
+
+        //When conversion mode is "to"
+        presenter.convertAmount("64","to")
+
+        //Then the to field should be updated
+        verify(view).updateFromField(any())
+
     }
 }
